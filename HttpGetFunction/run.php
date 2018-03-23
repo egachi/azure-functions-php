3b0684e5fa4856a39e8c8ad0c4ed8dc594adfd1b
mredison <egachi@live.com>
2018-03-23 11:17:24 -0600

    Updating

--- HttpGetFunction/run.php
+++ HttpGetFunction/run.php
@@ -1,7 +1,21 @@
 <?php
     echo "My fist function";
-    $body = file_get_contents(getenv('req'));
+    $body = file_get_contents(getenv('name'));
     $body = rtrim($body, "\n\r");
     fwrite(STDOUT, "PHP script processed request body '$body'\r\n");
-    file_put_contents("{ \"JSON\" : \"JSON Details\" } ");
+    file_put_contents("something","{ \"JSON\" : \"JSON Details\" } ");
+
+
+      /* request body */
+//   $body = file_get_contents(getenv('req'));
+//   $body = rtrim($body, "\n\r");
+//   fwrite(STDOUT, "PHP script processed request body '$body'\r\n");
+
+//   /* access query parameter 'test' */
+//   $test = getenv('req_query_test');
+//   fwrite(STDOUT, "Parameter Test='$test'\r\n");
+
+//   /* access header 'test' */
+//   $test = getenv('req_headers_test');
+//   fwrite(STDOUT, "Header Test='$test'\r\n");
 ?>
