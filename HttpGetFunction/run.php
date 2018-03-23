bd4ff443b10b8699df5f84fe6a5ba9a301d1118e
mredison <egachi@live.com>
2018-03-23 11:10:10 -0600

    Adding post code

--- HttpGetFunction/run.php
+++ HttpGetFunction/run.php
@@ -1,3 +1,6 @@
 <?php
     echo "My fist function";
+    $body = file_get_contents(getenv('req'));
+    $body = rtrim($body, "\n\r");
+    fwrite(STDOUT, "PHP script processed request body '$body'\r\n");
 ?>
