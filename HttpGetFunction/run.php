bd62d7bf0523e106c819c2688b3aec7aa8989892
mredison <egachi@live.com>
2018-03-23 11:13:38 -0600

    Updating function

--- HttpGetFunction/run.php
+++ HttpGetFunction/run.php
@@ -3,4 +3,5 @@
     $body = file_get_contents(getenv('req'));
     $body = rtrim($body, "\n\r");
     fwrite(STDOUT, "PHP script processed request body '$body'\r\n");
+    file_put_contents("{ \"JSON\" : \"JSON Details\" } ");
 ?>
