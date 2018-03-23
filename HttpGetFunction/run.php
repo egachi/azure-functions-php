0ef6a32b5e7f60976b5ff29de75fe893747e4f5b
mredison <egachi@live.com>
2018-03-23 14:06:50 -0600

    Updating function with timeout

--- HttpGetFunction/run.php
+++ HttpGetFunction/run.php
@@ -1,19 +1,44 @@
 <?php
-    //echo "Printing in console";
-    
-    //$name = file_get_contents(getenv('name'));
-    //$age =  file_get_contents(getenv('age'));
+    //Sample custom php function
+    function print_Json($name, $age, $id)
+    {
+        return "{ \"name\" : '$name', \"age\" : '$age',  \"user_id\" : '$id'} ";
+    }
 
-    echo "Printing in console from echo";
-    //retrieve original GET string 
-    $getReqString = getenv('REQ_QUERY'); 
-    //remove the ? for the parse_str function $getReqString = substr($getReqString,1,strlen($getReqString)); 
-    //convert the GET string to an array 
-    $parsedRequest = array(); parse_str($getReqString,$parsedRequest); 
-    //show contents of the new array
-    //show the value of a GET variable 
-    $name = $parsedRequest["name"];
-    $age =  $parsedRequest["age"];
-    fwrite(STDOUT, "How to write in console '$name' + '$age'");
-    file_put_contents(getenv('res'),"{ \"name\" : \"'$name'\", \"age\" : '$age' } ");
+    //Printing variables in console
+    echo "Printing in console with echo \r\n";
+    fwrite(STDOUT, "Printing in console with fwrite \r\n");
+    print_r("Printing in console with print_r \r\n");
+
+    //Getting REQ variables
+    $req_original_url = getenv('REQ_ORIGINAL_URL');
+    print_r("REQ_ORIGINAL_URL: '$req_original_url' \r\n");
+    $req_method =  getenv('REQ_METHOD');
+    print_r("REQ_METHOD: '$req_method' \r\n");
+    $completed_querystring = getenv('REQ_QUERY'); 
+    print_r("REQ_QUERY: '$completed_query' \r\n");
+
+    //Getting Query variables
+    $name = getenv('REQ_QUERY_name');
+    print_r("name: '$name' \r\n");
+    $age = getenv('REQ_QUERY_age');
+    print_r("age: '$age' \r\n");
+
+    //Getting Request headers
+    $sample_header = getenv('REQ_HEADERS_X-Sample-Test');
+    print_r("X-Sample-Test: '$sample_header' \r\n");
+
+    $id  = getenv('REQ_PARAMS_id');
+    print_r("USER_ID: '$id' \r\n");
+
+    $echo_time = time();
+    $interval = 47*60;
+    while(true){
+            if ($echo_time + $interval >= time()){
+                echo "$interval seconds have passed...";
+                $echo_time = time(); // set up timestamp for next interval
+            }
+        }
+    //How to send response, this for JSON Object content type.
+    file_put_contents(getenv('res'),print_Json($name,$age,$id));
 ?>
